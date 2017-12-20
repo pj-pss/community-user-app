a22364fb6718374d38cf137d0f8fc9fd2677d149
Tsukahara <tsukahara.takut@jp.fujitsu.com>
2017-12-20 16:51:52 +0900

    Add a link to the table row

--- src/html/js/user.js
+++ src/html/js/user.js
@@ -778,3 +778,16 @@ $(function () {
         $(".slide-menu").removeClass('slide-on');
     });
 });
+
+
+$(function($) {
+    $('tbody tr[data-href]').addClass('clickable').click( function() {
+        window.location = $(this).attr('data-href');
+    }).find('a').hover( function() {
+        $(this).parents('tr').unbind('click');
+    }, function() {
+        $(this).parents('tr').click( function() {
+            window.location = $(this).attr('data-href');
+        });
+    });
+});
